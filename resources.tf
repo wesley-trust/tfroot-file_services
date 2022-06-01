b1c9ea09619894f5600be409a97207b10b06a460
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2022-06-01 14:24:40 +0100

    Correct arguments

--- resources.tf
+++ resources.tf
@@ -1,4 +1,4 @@
-/* module "file_services" {
+module "file_services" {
   for_each                  = toset(local.resource_locations)
   source                    = "github.com/wesley-trust/tfmodule-compute"
   service_environment       = terraform.workspace
@@ -14,19 +14,16 @@
   resource_dns_servers      = lookup(var.resource_dns_servers, each.value, null)
   resource_network_role     = var.resource_network_role
   operating_system_platform = var.operating_system_platform
-} */
+}
 
-/* module "file_services_network_peering" {
+module "file_services_network_peering" {
   for_each                   = toset(local.resource_locations)
   source                     = "github.com/wesley-trust/tfmodule-network_peering"
   service_environment        = terraform.workspace
-  service_deployment         = var.service_deployment
-  service_name               = var.service_name
-  service_location           = each.value
   resource_network_peer      = module.file_services[each.value].network_name
   resource_group_peer        = module.file_services[each.value].resource_group_name
   resource_network_peer_role = var.resource_network_peer_role
-} */
+}
 
 /* module "file_services_storage_sync" {
   for_each               = toset(local.resource_storage_sync_locations)
@@ -52,7 +49,7 @@
   resource_recovery_services_virtual_machines = module.file_services[each.value]
 } */
 
-/* module "file_services_bcdr" {
+module "file_services_bcdr" {
   for_each                  = toset(local.resource_bcdr_locations)
   source                    = "github.com/wesley-trust/tfmodule-compute"
   service_environment       = terraform.workspace
@@ -68,16 +65,13 @@
   resource_dns_servers      = lookup(var.resource_dns_servers, each.value, null)
   resource_network_role     = var.resource_network_role
   operating_system_platform = var.operating_system_platform
-} */
+}
 
-/* module "file_services_network_peering_bcdr" {
+module "file_services_network_peering_bcdr" {
   for_each                   = toset(local.resource_bcdr_locations)
   source                     = "github.com/wesley-trust/tfmodule-network_peering"
   service_environment        = terraform.workspace
-  service_deployment         = var.service_deployment
-  service_name               = var.service_name
-  service_location           = each.value
   resource_network_peer      = module.file_services_bcdr[each.value].network_name
   resource_group_peer        = module.file_services_bcdr[each.value].resource_group_name
   resource_network_peer_role = var.resource_network_peer_role
-} */
+}
