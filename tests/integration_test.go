43dc21af41a072e08ccc9275b5360e04a20606d3
Wesley Trust <18400710+wesley-trust@users.noreply.github.com>
2022-06-07 18:36:00 +0100

    Change destroy

--- tests/integration_test.go
+++ tests/integration_test.go
@@ -43,7 +43,7 @@ func TestApplyDefault(t *testing.T) {
 	})
 
 	// At the end of the test, run `terraform destroy` to clean up any resources that were created
-	defer terraform.Destroy(t, terraformOptions)
+	defer terraform.DestroyE(t, terraformOptions)
 
 	// At the end of the test, run `terraform destroy` again, in case failures leave orphaned resources
 	defer terraform.Destroy(t, terraformOptions)
